1c7d52acd45fa8bb1f6b70b4ef2fd831daa10e13
criskars <59507050+criskars@users.noreply.github.com>
2020-01-04 21:30:31 -0300

    Update settings.py

--- PESSOAL___controle_de_gastos/settings.py
+++ PESSOAL___controle_de_gastos/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e48b1a72-938c-41bb-ace8-b7e0e27b5b53'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
